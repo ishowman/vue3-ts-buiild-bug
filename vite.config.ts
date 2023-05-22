c606d39944dff08d991f7a00716b08267ec37566
Devin <Devin@qq.com>
2023-05-22 18:27:18 +0800

    chore: add unplugin-auto-import

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
-
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,6 +20,8 @@ export default defineConfig({
         filepath: './configs/.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
       },
      }),
+     Components({
+    }),
 
   ],
   resolve: {
